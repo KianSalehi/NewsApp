0f0abc4357ae184eb2dec0f5ecca49c13a63f0aa
KianSalehi <kian.k.salehi@gmail.com>
2021-05-19 14:15:20 -0400

    scraper instagram updated

--- instagram/instagramPost.js
+++ instagram/instagramPost.js
@@ -1,10 +1,14 @@
 const Instagram = require('instagram-web-api');
 const credentials = require('credentials.json');
+const getDataFromDB = require('../database/getDataFromDB');
 const user = credentials.user;
 const pass = credentials.password;
 const client = new Instagram({user, pass});
 
 function instagramPost(){
+    getDataFromDB.fromNBC();
+    getDataFromDB.fromCNN();
+    getDataFromDB.fromBBC();
 
 }
 
--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,14 +1,17 @@
+const sendDataToDB = require('../database/sendDataToDB');
 // Get bbc data
-function getBBC(){
+function getBBC() {
 
+    sendDataToDB.sendBBC();
 }
 // Get nbc data
 function getNBC(){
 
+    sendDataToDB.sendNBC();
 }
 // Get cnn data
 function getCNN(){
 
+    sendDataToDB.sendCNN();
 }
-
 module.exports={getBBC, getCNN, getNBC}
