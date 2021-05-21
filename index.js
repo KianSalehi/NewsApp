86f0e3d1d62c64b8dbf98dca06e82597d2dde788
KianSalehi <kian.k.salehi@gmail.com>
2021-05-20 21:07:11 -0400

    NBC news scraper added

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 //const instagramPost = require('instagram/instagramPost');
 const scrape = require('./scraper/scrape.js');
 const data = require('./database/getDataFromDB');
-//data.fromBBC();
-scrape.getBBC();
+//scrape.getBBC();
 //scrape.getCNN();
 //scrape.getNBC();
 //instagramPost.instagramPost();
--- scraper/scrape.js
+++ scraper/scrape.js
@@ -11,7 +11,7 @@ function getBBC() {
             const $ = cheerio.load(html);
             let newsTable=$('.gel-layout__item > li');
             let topNews=[];
-            newsTable.each(function (index,value){
+            newsTable.each(function (){
                 const title = $(this).find('.gs-c-promo-heading > span').text();
                 const link = "https://www.bbc.com"+ $(this).find('.gs-o-media__body > a').attr("href");
                 topNews.push({
@@ -26,30 +26,30 @@ function getBBC() {
         });
 }
 // Get nbc data
-/*
+
 function getNBC(){
-    const url = 'https://www.bbc.com/news'
+    const url = 'https://www.nbcnews.com/'
     axios(url)
         .then(response=>{
             const html = response.data;
             const $ = cheerio.load(html);
-            let newsTable=$('.gel-layout__item > li');
+            let newsTable=$('.alacarte__items > li');
             let topNews=[];
-            newsTable.each(function (index,value){
-                const title = $(this).find('.gs-c-promo-heading > span').text();
-                const link = "https://www.bbc.com"+ $(this).find('.gs-o-media__body > a').attr("href");
+            newsTable.each(function (){
+                const title = $(this).find('.alacarte__headline').text();
+                const link = $(this).find('.alacarte__text-wrapper > a').attr("href");
                 topNews.push({
                     title: title,
                     link: link
                 });
             });
-            sendDataToDB.sendBBC(topNews);
+            sendDataToDB.sendNBC(topNews);
             console.log(topNews);
 
 
         });
 }
-// Get cnn data
+// Get cnn data, need to finish this
 function getCNN(){
     const url = 'https://www.bbc.com/news'
     axios(url)
@@ -71,5 +71,5 @@ function getCNN(){
 
 
         });
-}*/
+}
 module.exports={getBBC, getCNN, getNBC}
